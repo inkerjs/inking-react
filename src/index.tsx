80462e5933f9f74d6f4cb76c0e1eae66fbe0b9eb
Wee <fi3ework@gmail.com>
2019-04-04 01:08:57 +0800

    feat: in favour of redux-like props mapper function

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,15 @@
+import { autorun, reaction } from 'inking'
 import React from 'react'
+
+export function useForceUpdate() {
+  const [, setTick] = React.useState(0)
+
+  const update = React.useCallback(() => {
+    setTick(tick => tick + 1)
+  }, [])
+
+  return update
+}
 
 const InkingContext = React.createContext({
   state: null,
@@ -14,5 +25,34 @@ export const createStore = (initialState, reducer) => ({
   useInking() {
     const context = React.useContext(InkingContext)
     return { state: context.state, dispatch: context.dispatch }
+  }
+})
+
+const InkingContext2 = React.createContext({
+  store: {}
+})
+
+export const createReactiveStore = store => ({
+  ReactiveProvider({ children }) {
+    return <InkingContext2.Provider value={{ store }}>{children}</InkingContext2.Provider>
+  },
+  useReactive(mapperFn: any) {
+    const context = React.useContext(InkingContext2)
+    const contextStore = context.store
+    const [, setTick] = React.useState(0)
+    reaction(
+      () => {
+        const boundRes = mapperFn(store)
+        Object.keys(boundRes).forEach(key => {
+          if (typeof contextStore[key] === 'function') {
+            contextStore[key] = contextStore[key].bind(contextStore)
+          }
+        })
+      },
+      () => {
+        setTick(tick => tick + 1)
+      }
+    )
+    return mapperFn(store)
   }
 })
