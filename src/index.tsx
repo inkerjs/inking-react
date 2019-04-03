6ee50f2876104f8a1547b4644ad6dfa5098b66a5
Wee <fi3ework@gmail.com>
2019-04-03 22:46:07 +0800

    feat: use useContext to trigger re-render

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,18 @@
 import React from 'react'
+
+const InkingContext = React.createContext({
+  state: null,
+  dispatch: null
+})
+
+export const createStore = (initialState, reducer) => ({
+  Provider({ children }) {
+    const [state, dispatch] = React.useReducer(reducer, initialState)
+    const contextValue = { state, dispatch }
+    return <InkingContext.Provider value={contextValue as any}>{children}</InkingContext.Provider>
+  },
+  useInking() {
+    const context = React.useContext(InkingContext)
+    return { state: context.state, dispatch: context.dispatch }
+  }
+})
---
+++ src/useStore.tsx
@@ -0,0 +1,18 @@
+import { useCallback, useState } from 'react'
+
+export function useForceUpdate() {
+  const [, setTick] = useState(0)
+
+  const update = useCallback(() => {
+    setTick(tick => tick + 1)
+  }, [])
+
+  return update
+}
+
+export default function useStore(store, mapState) {
+  const update = () => {}
+  const autorun(() => {
+    state
+  })
+}
